99331d4a1135d202193b3778c5838f7cacee75a8
Сергей «kezzyhko» Семушин <kezzyhko@gmail.com>
2019-06-15 15:35:25 +0000

    Initial commit

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] GameObject objectToFollow;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = this.transform.position;
+        newPosition.x = objectToFollow.transform.position.x;
+        newPosition.y = objectToFollow.transform.position.y;
+        transform.position = newPosition;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rigidBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            rigidBody.AddForce(Vector3.up * 25000);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward);
+            rigidBody.AddForce(Vector3.left * 1000);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward);
+            rigidBody.AddForce(Vector3.right * 1000);
+        }
+
+    }
+}
